e8a03b9cb64151de5581d57e8ed2d710838b039e
Carlos-mc14 <1137440@senati.pe>
2025-09-23 19:41:45 -0500

    fix: update getBaseUrl function to return correct server URL based on environment variables

--- lib/api/food-events.ts
+++ lib/api/food-events.ts
@@ -8,9 +8,7 @@ function getBaseUrl() {
     return ""
   }
   // Server-side
-  return process.env.NEXT_PUBLIC_APP_URL || process.env.VERCEL_URL
-    ? `https://${process.env.VERCEL_URL}`
-    : "http://localhost:3000"
+  return process.env.NEXT_PUBLIC_APP_URL
 }
 
 export async function getFoodEvents(options?: {
